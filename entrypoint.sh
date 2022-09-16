a3978a97bffe1aee2926acbef6fcd15cabff61fd
Daniel <djperrefort@gmail.com>
2022-09-16 13:42:11 -0400

    Remove password from slurm database

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,6 @@ done
 echo "Creating Slurm account database..."
 mysql -NBe "CREATE DATABASE slurm_acct_db"
 mysql -NBe "CREATE USER 'slurm'@'localhost'"
-mysql -NBe "SET PASSWORD for 'slurm'@'localhost' = password('password')"
 mysql -NBe "GRANT USAGE ON *.* to 'slurm'@'localhost'"
 mysql -NBe "GRANT ALL PRIVILEGES on slurm_acct_db.* to 'slurm'@'localhost'"
 mysql -NBe "FLUSH PRIVILEGES"
