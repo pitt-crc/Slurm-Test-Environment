13bdabb1d0d50c5d873a3bf00315f56f2bde6ce3
Daniel Perrefort <djperrefort@gmail.com>
2022-09-16 15:37:40 -0400

    Add mock slurm accounts to entrypoint script (#30)
    * Add mock slurm accounts to entrypoint script
    
    * Remove password from slurm database
    
    * Add /var/slurmstate directory
    
    * Add sleep to wait for slurmctld
    
    * Update node addresses in conf files
    
    * Lengthen sleep when waiting for slurmctld
    
    * Revert "Lengthen sleep when waiting for slurmctld"
    
    This reverts commit b07d1869e2d63231a6bb4a58b714f12284cfd91e.
    
    * More sleep statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,11 @@ set -eo pipefail
 
 echo "Launching sql server..."
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
-
-# Wait for mysql to start up
-for i in {30..0}; do
-  if echo "SELECT 1" | mysql &>/dev/null; then
-    break
-  fi
-  sleep 1
-done
+sleep 3 # Wait for mysql to start up
 
 echo "Creating Slurm account database..."
 mysql -NBe "CREATE DATABASE slurm_acct_db"
 mysql -NBe "CREATE USER 'slurm'@'localhost'"
-mysql -NBe "SET PASSWORD for 'slurm'@'localhost' = password('password')"
 mysql -NBe "GRANT USAGE ON *.* to 'slurm'@'localhost'"
 mysql -NBe "GRANT ALL PRIVILEGES on slurm_acct_db.* to 'slurm'@'localhost'"
 mysql -NBe "FLUSH PRIVILEGES"
@@ -28,8 +20,16 @@ echo "Starting munge..."
 
 echo "Starting slurmdbd..."
 /usr/sbin/slurmdbd
+sleep 3 # Wait for slurmdbd to start up
 
 echo "Starting slurmctld..."
-/usr/sbin/slurmctld
+mkdir /var/slurmstate
+chown slurm /var/slurmstate
+/usr/sbin/slurmctld -c
+sleep 3 # Wait for slurmctld to start up
+
+echo "Creating mock user accounts..."
+sacctmgr -i add account "account1" description="account1_desc" organization="account1_org"
+sacctmgr -i add account "account2" description="account2_desc" organization="account2_org"
 
 exec "$@"
